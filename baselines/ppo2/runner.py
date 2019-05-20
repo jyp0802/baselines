fdc675921e3b7a5ea932e6aa7c79c91ba5820563
micahcarroll <micah.d.carroll@gmail.com>
2019-05-20 11:30:06 -0700

    Added traj level self-play annealing

--- baselines/ppo2/runner.py
+++ baselines/ppo2/runner.py
@@ -29,25 +29,13 @@ class Runner(AbstractEnvRunner):
         int_time = 0
         num_envs = len(self.curr_state)
 
+        if self.env.trajectory_sp:
+            # Selecting which environments should run fully in self play
+            sp_envs_bools = np.random.random(num_envs) < self.env.self_play_randomization
+            print("SP envs: {}/{}".format(sum(sp_envs_bools), num_envs))
+
         other_agent_simulation_time = 0
         for _ in range(self.nsteps):
-
-            # if self.env.resolve_other_agents:
-            #     running_returns = 0
-            #     running_dones = []
-            #     running_infos = []
-            #     for agent in self.env.other_agents:
-            #         # switched_obs = [self.env.switch_player(o, agent.player_idx) for o in self.obs]
-            #         other_agent_action = agent.direct_action(self.obs)
-            #         self.obs[:], rewards, dones, infos = self.env.step(other_agent_action)
-            #         print(dones)
-            #         print(infos)
-            #         if dones:
-            #             break #?
-            #         # TODO: still doesn't fix using this extra info here
-            #         running_dones.append(dones)
-            #         running_returns += rewards
-            #         running_infos.append(infos)
 
             # Given observations, get action value and neglopacs
             # We already have self.obs because Runner superclass run self.obs[:] = env.reset() on init
@@ -62,34 +50,47 @@ class Runner(AbstractEnvRunner):
                     if self.env.other_agent_true:
                         p_self_play = self.env.self_play_randomization
 
-                        other_agent_actions = np.zeros_like(self.curr_state)
+                        if self.env.trajectory_sp:
 
-                        if p_self_play < 1:
-                            # Get actions through the action method of the agent
-                            from hr_coordination.mdp.overcooked_mdp import Action
-                            other_agent_actions = self.env.other_agent.action(self.curr_state, self.other_agent_idx)
-                            other_agent_actions = [Action.ACTION_TO_INDEX[a] for a in other_agent_actions]
+                            if sum(sp_envs_bools) != num_envs: # Compute BC actions if some num of envs not running in BC
+                                # Get actions through the action method of the agent
+                                from hr_coordination.mdp.overcooked_mdp import Action
+                                other_agent_actions_bc = self.env.other_agent.action(self.curr_state, self.other_agent_idx)
+                                other_agent_actions_bc = [Action.ACTION_TO_INDEX[a] for a in other_agent_actions_bc]
 
-                        ####### Naive non-parallelized way of getting actions for other ########
-                        
-                        # other_agent_actions = []
-                        # for i in range(num_envs):
-                        #     s = self.curr_state[i]
-                        #     a_idx = self.other_agent_idx[i]
-                        #     self.env.other_agent.set_agent_index(a_idx)
-                        #     action = self.env.other_agent.action(s)
-                        #     other_agent_actions.append(Action.ACTION_TO_INDEX[action])
-                        
-                        if p_self_play > 0:
-                            self_play_actions, _, _, _ = self.model.step(self.obs1, S=self.states, M=self.dones)
-                            self_play_bools = np.random.random(num_envs) < p_self_play
+                            if sum(sp_envs_bools) != 0: # Compute SP actions if some num of envs is supposed to run in SP
+                                other_agent_actions_sp, _, _, _ = self.model.step(self.obs1, S=self.states, M=self.dones)
 
-                            # Currently randomizes at the step level, might want to randomize at the trajectory level
-                            # That is harder to do though, as requires dealing with dones
+                            other_agent_actions = []
                             for i in range(num_envs):
-                                is_self_play_action = self_play_bools[i]
-                                if is_self_play_action:
-                                    other_agent_actions[i] = self_play_actions[i]
+                                if sp_envs_bools[i]:
+                                    sp_action = other_agent_actions_sp[i]
+                                    other_agent_actions.append(sp_action)
+                                else:
+                                    bc_action = other_agent_actions_bc[i]
+                                    other_agent_actions.append(bc_action)
+                        
+                        else:
+                            other_agent_actions = np.zeros_like(self.curr_state)
+
+                            if p_self_play < 1:
+                                # Get actions through the action method of the agent
+                                from hr_coordination.mdp.overcooked_mdp import Action
+                                other_agent_actions = self.env.other_agent.action(self.curr_state, self.other_agent_idx)
+                                other_agent_actions = [Action.ACTION_TO_INDEX[a] for a in other_agent_actions]
+
+                            ####### Naive non-parallelized way of getting actions for other ########
+                            
+                            if p_self_play > 0:
+                                self_play_actions, _, _, _ = self.model.step(self.obs1, S=self.states, M=self.dones)
+                                self_play_bools = np.random.random(num_envs) < p_self_play
+
+                                # Currently randomizes at the step level, might want to randomize at the trajectory level
+                                # That is harder to do though, as requires dealing with dones
+                                for i in range(num_envs):
+                                    is_self_play_action = self_play_bools[i]
+                                    if is_self_play_action:
+                                        other_agent_actions[i] = self_play_actions[i]
 
                     elif self.env.other_agent_bc:
                         # Parallelise actions with direct action, using the featurization function
@@ -115,13 +116,6 @@ class Runner(AbstractEnvRunner):
             mb_values.append(values)
             mb_neglogpacs.append(neglogpacs)
             mb_dones.append(self.dones)
-
-            # if self.env.joint_action:
-            #     partial = time.time()
-            #     observations = [switch_player(o) for o in self.obs]
-            #     other_agent_actions = self.env.other_agent.direct_action(observations)
-                
-            #     int_time += (time.time() - partial)
 
             # Take actions in env and look the results
             # Infos contains a ton of useful informations
